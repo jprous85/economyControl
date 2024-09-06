f985f41cbb43c9c9b3fcd946a52c6f14fe91c49a
jprous85 <jprous85@gmail.com>
2024-09-06 02:21:37 +0200

    change docker-compose file

--- docker/mysql/dropmysqltables.sh
+++ docker/mysql/dropmysqltables.sh
@@ -7,4 +7,10 @@ fi
 DATABASE_CONTAINER=$1
 DATABASE_NAME=$2
 
-docker exec "$DATABASE_CONTAINER" mysql --user="$DB_USERNAME" --password="$DB_PASSWORD" -Nse 'SHOW TABLES' 2>/dev/null "$DATABASE_NAME" | while read -r table; do docker exec "$DATABASE_CONTAINER" mysql --user="$DB_USERNAME" --password="$DB_PASSWORD" -Nse "SET FOREIGN_KEY_CHECKS=0;TRUNCATE TABLE $table;SET FOREIGN_KEY_CHECKS=1;" 2>/dev/null "$DATABASE_NAME"; done
+docker exec "$DATABASE_CONTAINER" mysql --user="$DB_USERNAME" --password="$DB_PASSWORD" -Nse 'SHOW TABLES' 2>/dev/null "$DATABASE_NAME" |
+while read -r table;
+do
+    docker exec "$DATABASE_CONTAINER" mysql --user="$DB_USERNAME" --password="$DB_PASSWORD" \
+    -Nse "SET FOREIGN_KEY_CHECKS=0;TRUNCATE TABLE $table;SET FOREIGN_KEY_CHECKS=1;" 2>/dev/null \
+    "$DATABASE_NAME";
+done
